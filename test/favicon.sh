b23e01f96badf1c7607d59c2da6f0e5c10005012
Alper Yazar <alperyazar@gmail.com>
2020-03-15 14:57:58 +0300

    modify favicon.ico location after it is fixed by the cinder theme. Enhance the favicon.ico unit test md5 check

--- test/favicon.sh
+++ test/favicon.sh
@@ -3,7 +3,8 @@
 # Test for a valid favicon.ico at main directory
 
 TESTNAME="favicon test"
-FILE="${SITEDIR}/favicon.ico"
+FILE="${SITEDIR}/img/favicon.ico"
+MD5SUMexpected="ebb6340ce1e2157b2ebedfe84555a013"
 
 echo "STARTING: ${TESTNAME}"
 echo "Looking for ${FILE}"
@@ -12,6 +13,13 @@ if [ ! -s "$FILE" ]; then
 	echo "$FILE does not exist OR its size is 0"
 	echo "ERROR: ${TESTNAME}"
 	exit 1
+fi
+
+MD5SUM=`md5sum $FILE | awk '{ print $1 }'`
+
+if [ "$MD5SUMexpected" != "$MD5SUM" ]; then
+	echo "ERROR: Different $FILE exists"
+	echo "Expected MD5 hash is $MD5SUMexpected, but got $MD5SUM"
 fi
 
 echo "DONE: ${TESTNAME}"
